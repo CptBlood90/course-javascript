cb7f393cd22be4d8a0a4f4608e80a85c2eef98b9
Evgeniy Oleynikov <mrstag90@gmail.com>
2023-01-26 15:19:49 +0300

    Third task modified

--- projects/exceptions/index.js
+++ projects/exceptions/index.js
@@ -16,20 +16,21 @@
    isAllTrue([1, 2, 3, 4, 5], n => n < 10) // вернет true
    isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false
  */
-function isAllTrue(array, fn) {
-  if (typeof fn !== 'function') {
-    throw new Error('fn is not a function');
-  }
-  if (!Array.isArray(array) || !array.length) {
-    throw new Error('empty array');
-  }
-  for (const el of array) {
-    if (!fn(el)) {
-      return false;
+   function isAllTrue(array, fn) {
+    if (typeof fn !== 'function') {
+      throw new Error('fn is not a function');
     }
+  
+    if (!(array instanceof Array) || !array.length) {
+      throw new Error('empty array');
+    }
+    for (const el of array) {
+      if (!fn(el)) {
+        return false;
+      }
+    }
+    return true;
   }
-  return true;
-}
 
 /*
  Задание 2:
@@ -47,22 +48,23 @@ function isAllTrue(array, fn) {
    isSomeTrue([1, 2, 30, 4, 5], n => n > 20) // вернет true
    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
  */
-function isSomeTrue(array, fn) {
-  if (typeof fn !== 'function') {
-    throw new Error('fn is not a function');    
-  }
-
-  if (!Array.isArray(array) || !array.length) {
-    throw new Error('empty array'); 
+   function isSomeTrue(array, fn) {
+    if (typeof fn !== 'function') {
+      throw new Error('fn is not a function');
     }
-
+  
+    if (!(array instanceof Array) || !array.length) {
+      throw new Error('empty array');
+    }
+  
     for (const el of array) {
       if (fn(el)) {
         return true;
       }
     }
+  
     return false;
-}
+  }
 
 /*
  Задание 3:
